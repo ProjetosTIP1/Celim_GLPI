4daa37742c09604dc141071f2813eac94c8328ec
PEDREIRA\raisson.charles <raissoncharles@gmail.com>
2023-08-11 11:02:25 -0300

    V3.0 chamados pendentes

--- Celim_GLPI.py
+++ Celim_GLPI.py
@@ -33,7 +33,7 @@ sys.path.insert(1, CaminhoLib)
 from GerarChave import GerarChave
 from GerarVar import GerarVar
 from GerarLog import Gerarlog
-# from GerenciadorTarefas import GerenciadorTarefas
+from GerenciadorTarefas import GerenciadorTarefas
 # from FinalizarExecucao import FinalizarExecucao
 
 
@@ -43,7 +43,7 @@ BotVar.chaveBD = BotChave.gerarChave(chave)
 BotVar.getParametros(15)# 1 para falar o id do rpa para pegar os parametros
 # BotAreaTransferencia = AreaTransferencia()
 BotLog = Gerarlog(BotVar)
-# BotTarefas = GerenciadorTarefas(BotVar,BotLog)
+BotTarefas = GerenciadorTarefas(BotVar,BotLog)
 # BotFinalizar = FinalizarExecucao(BotVar,BotLog,BotTarefas)
 
 
@@ -79,6 +79,7 @@ class GLPI:
         self.limiteaguardarelemento = 0 # contador limitar as tentativas de procurar um elemento html
         self.tempo = 300 # tempo em segundos entre cada verificação
         self.limiteaguardartitulo = 0
+        self.limiteaguardartitulomaximo = 60
         self.conterro = 0 # contador para mandar mensagem somente se o erro acontecer mais de duas vezes seguida
     def relogio_timer(self,tempo):
         BotLog.imprimirLog("########################################################### INICIANDO MODULO RELOGIO_TIMER ###########################################################")
@@ -143,14 +144,16 @@ class GLPI:
         BotLog.imprimirLog("########################################################### INICIANDO MODULO ESPERAR TITULO ###########################################################")
         BotLog.imprimirLog("Esperando a janela com titulo: "+titulo)
         if titulo in pyautogui.getActiveWindow().title:
+            BotLog.imprimirLog("Encontrou o titulo, titulo atual: "+str(pyautogui.getActiveWindow().title))
             self.limiteaguardartitulo=0
             BotLog.imprimirLog("########################################################### FINALIZANDO MODULO ESPERAR TITULO ###########################################################")
             return True
-        if self.limiteaguardartitulo>60:
+        if self.limiteaguardartitulo>self.limiteaguardartitulomaximo:
             raise Exception("Execeu o limite de tentativas de encontrar o titulo "+titulo)
         time.sleep(1)
         self.limiteaguardartitulo+=1
-        bot.esperarTitulo(titulo)
+        if bot.esperarTitulo(titulo):
+            return True
     def limparDescricao(self,descricao):
         x = descricao.find('&#60;')
         if x == -1:
@@ -170,7 +173,8 @@ class GLPI:
                 descricao_limpa = bot.limparDescricao(descricao_limpa)
                 return descricao_limpa
     def tempoComercial(self,datastr):
-        BotLog.imprimirLog("########################################################### INICIANDO MODULO TEMPO COMERCIAL ###########################################################")
+        # BotLog.imprimirLog("########################################################### INICIANDO MODULO TEMPO COMERCIAL ###########################################################")
+        # Lendo a tabela de feriados
         sql_holidays = f"""
             SELECT id, name, begin_date, end_date, is_perpetual
             FROM glpi_holidays
@@ -179,21 +183,41 @@ class GLPI:
         conMySQLholidays = MySQLdb.connect(host=BotVar.serverMySQL,user=BotVar.usermysql,passwd=BotVar.senhamysql,db='glpi') #Criando a conexão
         dfholidays= pd.read_sql_query(sql_holidays,conMySQLholidays)
         BotLog.gerarExcel(dfholidays,'dfholidays'+datetime.datetime.today().strftime("%Y-%m-%d_%H.%M.%S"))
-
+        # Criando uma lista com os feriado
         self.feriados = []
         # self.chamadosvencendoavisado.append(str(numerodf))
         for x_holiday in range(len(dfholidays.index)):
             id = dfholidays['id'].iloc[x_holiday]
             name = dfholidays['name'].iloc[x_holiday]
-            is_perpetual = dfholidays['is_perpetual'].iloc[x_holiday]
             begin_data = dfholidays['begin_date'].iloc[x_holiday]
             end_date = dfholidays['end_date'].iloc[x_holiday]
+            is_perpetual = dfholidays['is_perpetual'].iloc[x_holiday]
+            if is_perpetual == 1: # condição para feriados perpetuos
+                if begin_data.year != end_date.year:
+                    msg = f"""Erro no Celim GLPI Chamados pendentes.
+                      Intervalo de datas na tabela de feriado com anos diferentes, favor corrigir.
+                      Data inicio: {begin_data.strftime("%d-%m-%Y")}
+                      Data Fim: {end_date.strftime("%d-%m-%Y")}
+                      Nome do feriado: {name}
+                      """
+                    msg = msg.replace("  ","")
+                    print(msg)
+                    BotVar.BotTelegram.send_message(int(self.id_telegram),msg)
+                if begin_data.year != datetime.datetime.now().date().year:
+                    # print("Ano de inicio do feriado diferente do ano atual, ajustando para o ano atual")
+                    begin_data = begin_data.replace(year=datetime.datetime.now().date().year)                        
+                if end_date.year != datetime.datetime.now().date().year:
+                    # print("Ano de inicio do feriado diferente do ano atual, ajustando para o ano atual")
+                    end_date = end_date.replace(year=datetime.datetime.now().date().year)
             if begin_data == end_date:
-                print("Datas de inicio e fim de feriado iguais")
+                # print("Datas de inicio e fim de feriado iguais")
                 self.feriados.append(begin_data)
-            elif begin_data > end_date: print("A data de id "+str(id)+' e nome '+str(name)+' na tabela de calendario esta com uma data de inicio maior que a data de fim.')
-            elif begin_data < end_date:
-                print("Datas diferentes, percorrendo as datas")
+            elif begin_data > end_date: # Inicio do feriado não pode ser maior que o fim
+                msg = "A data de id "+str(id)+' e nome '+str(name)+' na tabela de calendario esta com uma data de inicio maior que a data de fim.'
+                print(msg)
+                BotVar.BotTelegram.send_message(int(self.id_telegram),msg)
+            elif begin_data < end_date: #Adiciona o intervalo de datas do feriado
+                # print("Datas diferentes, percorrendo as datas")
                 difer_dias = end_date - begin_data
                 difer_dias = difer_dias.days
                 while begin_data <= end_date:
@@ -217,48 +241,69 @@ class GLPI:
         hoje_data = datetime.datetime.now().date()
         while date_time.date() <= datetime.datetime.now().date():
             dia_semana = date_time.weekday() 
-            if date.weekday() == 5 or date_time.weekday() == 6:
-                print("Final de semana")
+            if date_time.weekday() == 5 or date_time.weekday() == 6: # pular um dia se for final de semana
+                # print("Final de semana")
                 date_time = date_time + datetime.timedelta(days=1)
                 date_time = date_time.replace(hour=7,minute=0,second=0)
                 continue
-            if date_time.date() in self.feriados:
-                print("E um feriado")
+            if date_time.date() in self.feriados: # pular um dia se for feriado
+                # print("E um feriado")
                 date_time = date_time + datetime.timedelta(days=1)
                 date_time = date_time.replace(hour=7,minute=0,second=0)
                 continue
-            if (date_time.hour>17) or (date_time.hour==17 and date_time.minute>0):
+            if (date_time.weekday() == 4 and date_time.hour>16) or (date_time.weekday() == 4 and date_time.hour==17 and date_time.minute>0): # ir para o proximo dia se for mais de 16 horas e sexta
                 date_time = date_time + datetime.timedelta(days=1)
                 date_time = date_time.replace(hour=7,minute=0,second=0)
                 continue
-            if date_time.hour<7:
+            if (date_time.weekday() in (0,1,2,3) and date_time.hour>17) or (date_time.weekday() in (0,1,2,3) and date_time.hour==17 and date_time.minute>0): # ir para o proximo dia se for mais de 17 horas
+                date_time = date_time + datetime.timedelta(days=1)
                 date_time = date_time.replace(hour=7,minute=0,second=0)
-            if (date_time.hour != 7 or date_time.minute != 0) and (date_time.hour>=7) and (date_time.hour<17):
+                continue
+            if date_time.hour<7: # se o chamado for aberto antes das 7
+                date_time = date_time.replace(hour=7,minute=0,second=0)
+            if (date_time.hour != 7 or date_time.minute != 0) and (date_time.hour>=7) and (date_time.hour<17): # caucula diferença para o primeiro dia
                 fim_do_dia = date_time
-                fim_do_dia = fim_do_dia.replace(hour=7,minute=0,second=0)
+                fim_do_dia = fim_do_dia.replace(hour=17,minute=0,second=0)
                 diferenca = fim_do_dia - date_time
-                tempo_segundos+=(diferenca.hour() * 3600)
-                tempo_segundos+=(diferenca.minute() * 60)
-                tempo_segundos+=diferenca.seconds()
-                if date_time.weekday() == 5:
+                # tempo_segundos+=(diferenca.hour * 3600)
+                # tempo_segundos+=(diferenca.minute * 60)
+                tempo_segundos+=diferenca.seconds
+                if date_time.weekday() == 4: # tirar uma hora se for a sexta
                     tempo_segundos-=3600
-                date_time.hour = 7
-                date_time.minute = 0
+                date_time = date_time.replace(hour=7,minute=0)
                 date_time = date_time + datetime.timedelta(days=1)
                 continue
-            if date_time.date() == datetime.datetime.now().date():
-                diferenca = datetime.datetime.now() - date_time
-                tempo_segundos+=(diferenca.hour() * 3600)
-                tempo_segundos+=(diferenca.minute() * 60)
-                tempo_segundos+=diferenca.seconds()
+            if date_time.date() == datetime.datetime.now().date(): # diferença para hora atual, ultimo dia
+                hora_atual = datetime.datetime.now()
+                if (hora_atual.weekday() == 4 and hora_atual.hour > 16) or (hora_atual.weekday() == 4 and hora_atual.hour == 16 and hora_atual.minute > 0):
+                    hora_atual = hora_atual.replace(hour=16,minute=0)
+                if (hora_atual.weekday() in (0,1,2,3) and hora_atual.hour > 17) or (hora_atual.weekday() in (0,1,2,3) and hora_atual.hour == 17 and hora_atual.hour > 0):
+                    hora_atual = hora_atual.replace(hour=17,minute=0)
+                diferenca = hora_atual - date_time
+                # tempo_segundos+=(diferenca.hour * 3600)
+                # tempo_segundos+=(diferenca.minute * 60)
+                tempo_segundos+=diferenca.seconds
                 date_time = date_time + datetime.timedelta(days=1)
                 continue
-            tempo_segundos+=86400
+            if date_time.weekday() == 4: # se for sexta soma 9 horas
+                tempo_segundos+=32400
+            else: # se não for sexta soma dez horas
+                tempo_segundos+=36000
             date_time = date_time + datetime.timedelta(days=1)
 
-
-        print("")
-        BotLog.imprimirLog("########################################################### FINALIZANDO MODULO TEMPO COMERCIAL ###########################################################")
+        # BotLog.imprimirLog("########################################################### FINALIZANDO MODULO TEMPO COMERCIAL ###########################################################")
+        return tempo_segundos
+    def segundos_para_horas_minutos(self,segundos):
+        horas = segundos // 3600
+        minutos = (segundos % 3600) // 60
+        segundos = segundos % 60
+        if segundos < 9:
+            segundos = "0"+str(segundos)
+        if minutos < 9:
+            minutos = "0"+str(minutos)
+        if horas < 9:
+            horas = "0"+str(horas)
+        return str(horas)+":"+str(minutos)+":"+str(segundos)
     def novoChamado(self):
         BotLog.imprimirLog("########################################################### INICIANDO MODULO NOVO CHAMADO ###########################################################")
         
@@ -743,7 +788,7 @@ class GLPI:
                     , DATE_FORMAT(t.date, '%d-%m-%Y %T') Data_Abertura
                     , IFNULL(DATE_FORMAT(t.begin_waiting_date, '%d-%m-%Y %T'),"") Inicio_Espera
                     ,t.waiting_duration Tempo_Pendente_Segundos
-                    ,concat(HOUR(SEC_TO_TIME(t.waiting_duration)),":",LPAD(MINUTE(SEC_TO_TIME(t.waiting_duration)),2,0)) Tempo_Pendente_Horas
+                   -- ,concat(HOUR(SEC_TO_TIME(t.waiting_duration)),":",LPAD(MINUTE(SEC_TO_TIME(t.waiting_duration)),2,0)) Tempo_Pendente_Horas
                     , IFNULL(u2.name,"") Tecnico
                     , i.name Categoria
                 FROM glpi_tickets t
@@ -756,16 +801,40 @@ class GLPI:
             """
         conMySQLGLPI = MySQLdb.connect(host=BotVar.serverMySQL,user=BotVar.usermysql,passwd=BotVar.senhamysql,db='glpi') #Criando a conexão
         dfchamadospendente= pd.read_sql_query(sql,conMySQLGLPI)
+
+        dfchamadospendente['Tempo_Pendente_Segundos_Atual'] = 0
+        dfchamadospendente['Tempo_Pendente_Segundos_Total'] = 0
+        dfchamadospendente['Horas_Pendente'] = 0
+        total_chamados_pendentes = len(dfchamadospendente.index)
+        for x, data in enumerate(dfchamadospendente['Inicio_Espera']):
+            # data = dfchamadospendente['Inicio_Espera'].iloc[0]
+            tempo_segundos =  bot.tempoComercial(data)
+            dfchamadospendente['Tempo_Pendente_Segundos_Atual'].iloc[x] = tempo_segundos
+            tempo_pendente_total_Segundos = tempo_segundos + dfchamadospendente['Tempo_Pendente_Segundos'].iloc[x]
+            dfchamadospendente['Tempo_Pendente_Segundos_Total'].iloc[x] = tempo_pendente_total_Segundos
+            horas_pendentes = bot.segundos_para_horas_minutos(tempo_pendente_total_Segundos)
+            dfchamadospendente['Horas_Pendente'].iloc[x] = horas_pendentes
+        
+        nome_arquivo = 'dfchamados_pendentes_historico'+datetime.datetime.today().strftime("%Y-%m-%d_%H.%M.%S")
+        BotLog.gerarExcel(dfchamadospendente,nome_arquivo)
+
+        dfchamadospendente = dfchamadospendente.sort_values(['Tempo_Pendente_Segundos_Total'],ascending = False)
+        
+        ultima_linha = {'Numero':'Total de Chamados','Data_Abertura':total_chamados_pendentes}
+        dfchamadospendente.loc[total_chamados_pendentes+1]=ultima_linha
+        # dfchamadospendente = dfchamadospendente.append(ultima_linha)
+        dfchamadospendente = dfchamadospendente.drop(columns=['Tempo_Pendente_Segundos_Total'])
+        dfchamadospendente = dfchamadospendente.drop(columns=['Tempo_Pendente_Segundos_Atual'])
+        dfchamadospendente = dfchamadospendente.drop(columns=['Tempo_Pendente_Segundos'])
+
         nome_arquivo = 'dfchamados_pendentes_'+datetime.datetime.today().strftime("%Y-%m-%d_%H.%M.%S")
         BotLog.gerarExcel(dfchamadospendente,nome_arquivo)
         caminho_arquivo = r"log\\log"+BotLog.nomedir+"\\dataframe\\"+nome_arquivo+str(BotLog.contexcel)+".xlsx"
 
-
-
-        data = dfchamadospendente['Inicio_Espera'].iloc[0]
-        bot.tempoComercial(data)
-
-
+        BotLog.imprimirLog("Mudando o status para executando")
+        BotTarefas.MudarStatus("Executando")
+        BotLog.imprimirLog("Verificando prioridade")
+        BotTarefas.VerificaPrioridade()
 
         # Comando para abrir o arquivo Excel com o aplicativo Excel
         comando_excel = [self.caminho_excel, caminho_arquivo]
@@ -789,7 +858,14 @@ class GLPI:
 
         pyautogui.press('f12')
 
-        bot.esperarTitulo('Salvar como')
+        self.limiteaguardartitulo = 0
+        self.limiteaguardartitulomaximo = 10
+        if bot.esperarTitulo('Save As'):
+            print("Encontrou o titulo")
+        else:
+            self.limiteaguardartitulo = 0
+            self.limiteaguardartitulomaximo = 60
+            bot.esperarTitulo('Salvar como')
 
         nome_print = CaminhoProjeto+r"\log\log"+BotLog.nomedir+r"\print\print"+str(BotLog.contprint)+'.jpg'
         BotLog.contprint+=1
@@ -801,12 +877,17 @@ class GLPI:
         subprocess.call(["taskkill", "/S", 'localhost',  "/FI", "IMAGENAME eq mspaint*"])
         BotVar.BotTelegram.send_photo(int(bot.id_telegram),open(nome_print,'rb'))
 
+        BotLog.imprimirLog("Mudando o status para Parado")
+        BotTarefas.MudarStatus("Parado")
+
         BotLog.imprimirLog("########################################################### FINALIZANDO MODULO CHAMADOS PENDENTES ###########################################################")
        
+
 
     
 
 bot = GLPI()
+BotLog.InicioFim("InicioExecucao")
 
 # BotVar.dfparametros.loc[BotVar.dfparametros['NOME']=="id_telegram_alertas","VALOR"] = '452405307'
 # bot.id_telegram = '452405307'
@@ -814,10 +895,12 @@ bot = GLPI()
 
 
 
-# try:
-# bot.chamadosPendente()
-# except Exception as e:
-#     BotLog.imprimirLog(f"Erro no modulo chamadosPendentes: {e}")
+
+
+try:
+    bot.chamadosPendente()
+except Exception as e:
+    BotLog.imprimirLog(f"Erro no modulo chamadosPendentes: {e}")
 
 
 teste = datetime.datetime.now()
@@ -840,7 +923,7 @@ while bot.horariotermino>=datetime.datetime.now():
     bot.relogio_timer(bot.tempo)
     
 
-
+bot.InicioFim("FimExecucao")
 # BotFinalizar.finalizarExecucao("Finalizando execução com sucesso na ultima linha")
 
 
